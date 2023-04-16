358d29d1187c4341215e42c6ef3c6288874d6d8d
DhruvRajpoot <dhruv.kutarya@gmail.com>
2023-04-16 18:14:55 +0530

    feat: add playlist feature

--- src/pages/Playlists/Playlists.style.ts
+++ src/pages/Playlists/Playlists.style.ts
@@ -1,39 +1,119 @@
 import styled from "styled-components";
 
 export const PlaylistContainer = styled.div`
-    position: relative;
-    min-height: 100vh;
+  position: relative;
+  min-height: 100vh;
 `;
 
 export const AddMoreButton = styled.button`
-    position: absolute;
-    bottom : 3rem ;
-    right: 3rem;
-    width: 3rem;
-    height: 3rem;
-    background: #409040;
-    border: none;
-    outline: none;
+  position: absolute;
+  bottom: 3rem;
+  right: 3rem;
+  width: 3rem;
+  height: 3rem;
+  background: #409040;
+  border: none;
+  outline: none;
+  color: black;
+  font-size: 1.5rem;
+  cursor: pointer;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    bottom: 5rem;
+    right: 2rem;
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 0 3px 1px rgba(255, 255, 255, 0.5);
+    transition: all 0.2s ease-in-out;
+  }
+`;
+
+export const FormWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  display: none;
+  justify-content: center;
+  align-items: center;
+  width: calc(100vw - 240px);
+  min-height: 100vh;
+  background: rgba(0, 0, 0, 0.9);
+  z-index: 100;
+
+  @media (max-width: 768px) {
+    width: 100vw;
+  }
+`;
+
+export const Form = styled.form`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    width: 70%;
+  }
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 1rem;
+  font-size: 1.2rem;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  background: transparent;
+  color: #fff;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+`;
+
+export const SubmitButton = styled.button`
+  width: 30%;
+  padding: 0.7rem 1rem;
+  font-size: 1.2rem;
+  outline: none;
+  border: none;
+  background: #409040;
+  color: #fff;
+  cursor: pointer;
+  border-radius: 5px;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #308030;
+    color: #fff;
+  }
+
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+export const CloseButton = styled(SubmitButton)`
+  background: transparent;
+  border: 1px solid rgba(255, 255, 255, 0.7);
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.7);
     color: black;
-    font-size: 1.5rem;
-    cursor: pointer;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    @media (max-width: 768px) {
-        bottom: 5rem;
-        right: 2rem;
-    }
-
-    &:active {
-        transform: scale(.95);
-    }
-
-    &:hover {
-        transform: translateY(-5px);
-        box-shadow: 0 0 3px 1px rgba(255,255,255,0.5);
-        transition: all .2s ease-in-out;
-    }
+  }
 `;
--- src/pages/Playlists/Playlists.tsx
+++ src/pages/Playlists/Playlists.tsx
@@ -1,8 +1,17 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import useAxios from "../../utils/useAxios";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserPlaylist } from "../../Redux/reducers/playlistReducer";
-import { AddMoreButton, PlaylistContainer } from "./Playlists.style";
+import {
+  AddMoreButton,
+  ButtonWrapper,
+  CloseButton,
+  Form,
+  FormWrapper,
+  Input,
+  PlaylistContainer,
+  SubmitButton,
+} from "./Playlists.style";
 import { PlaylistRowList } from "../../Components/PlaylistRow/PlaylistRow.style";
 import PlaylistCard from "../../Components/PlaylistCard/PlaylistCard";
 import PlaylistRow from "../../Components/PlaylistRow/PlaylistRow";
@@ -14,18 +23,21 @@ const Playlists = () => {
   const api = useAxios();
   const dispatch = useDispatch();
   const userPlaylist = useSelector((state: any) => state.playlist.userPlaylist);
-  const user=useSelector((state:any)=>state.user.user);
-  const username=(Object.keys(user).length)!==0?user.display_name[0].toUpperCase()+user.display_name.slice(1).toLowerCase():null;
+  const user = useSelector((state: any) => state.user.user);
+  const username =
+    Object.keys(user).length !== 0
+      ? user.display_name[0].toUpperCase() +
+        user.display_name.slice(1).toLowerCase()
+      : null;
 
   const UserPlaylistFunc = async () => {
     setLoading(true);
-    try{
+    try {
       const res = await api.get(`/me/playlists`);
       dispatch(getUserPlaylist(res.data));
+    } catch (err: any) {
+      console.log(err.message);
     }
-    catch(err:any){
-      console.log(err.message)
-    };
     setLoading(false);
   };
 
@@ -33,15 +45,61 @@ const Playlists = () => {
     UserPlaylistFunc();
   }, []);
 
+  const formRef = useRef<HTMLDivElement>(null);
+  const handleOpen = (e: any) => {
+    formRef.current ? (formRef.current.style.display = "flex") : null;
+  };
+
+  const handleCancel = (e: any) => {
+    formRef.current ? (formRef.current.style.display = "none") : null;
+  };
+
+  const handleSubmit = async(e: any) => {
+    e.preventDefault();
+    const playlistName = e.target[0].value;
+
+    try{
+      await api.post(`/users/${user.id}/playlists`, {
+        name: playlistName,
+      });
+    }
+    catch(err: any){
+      console.log(err.message);
+    }
+
+    UserPlaylistFunc();
+    formRef.current ? (formRef.current.style.display = "none") : null;
+    e.target[0].value = "";
+  };
+
   return (
     <>
-    {
-      loading ? <Loader/> : 
-      <PlaylistContainer>
-      <PlaylistRow title={`${username?username:'Your'}'s Playlists`} data={userPlaylist} />
-      <AddMoreButton title="create playlist"><MdPlaylistAdd/></AddMoreButton>
-      </PlaylistContainer>
-    }
+      {loading ? (
+        <Loader />
+      ) : (
+        <PlaylistContainer>
+          <PlaylistRow
+            title={`${username ? username : "Your"}'s Playlists`}
+            data={userPlaylist}
+          />
+          <AddMoreButton title="create playlist" onClick={handleOpen}>
+            <MdPlaylistAdd />
+          </AddMoreButton>
+          <FormWrapper ref={formRef}>
+            <Form onSubmit={handleSubmit}>
+              <Input type="text" placeholder="Playlist Name" required/>
+              <ButtonWrapper>
+                <CloseButton type="reset" onClick={handleCancel}>
+                  Cancel
+                </CloseButton>
+                <SubmitButton type="submit">
+                  Create
+                </SubmitButton>
+              </ButtonWrapper>
+            </Form>
+          </FormWrapper>
+        </PlaylistContainer>
+      )}
     </>
   );
 };
